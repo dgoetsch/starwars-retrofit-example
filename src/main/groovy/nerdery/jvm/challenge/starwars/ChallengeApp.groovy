409d401049397d8cc5c3d71590587814538563e4
Devyn Goetsch <dgoetsch@nerdery.com>
2015-10-28 15:41:01 -0500

    make it concurrent to make it faster

--- src/main/groovy/nerdery/jvm/challenge/starwars/ChallengeApp.groovy
+++ src/main/groovy/nerdery/jvm/challenge/starwars/ChallengeApp.groovy
@@ -10,6 +10,13 @@ import nerdery.jvm.challenge.starwars.service.StarWarsService
 import retrofit.JacksonConverterFactory
 import retrofit.Retrofit
 
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executor
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+
 /**
  * Created by dgoetsch on 10/27/15.
  */
@@ -17,18 +24,36 @@ class ChallengeApp {
     static final String BASE_URL = "http://swapi.co"
 
     static void main(String ... args) {
-        Map<String, List<String>> starshipsByTatooineResident = [:]
+        Map<String, List<String>> starshipsByTatooineResident = new ConcurrentHashMap<>()
         StarWarsService service = new StarWarsService()
         Planet tatooine = service.getPlanetByName("Tatooine")
+        ExecutorService executor = Executors.newCachedThreadPool()
+        CountDownLatch tatooineLatch = new CountDownLatch(tatooine.residents.size())
         tatooine.residents.each({
-            Person person = service.getPersonFromUri(it)
-            starshipsByTatooineResident.put(person.name, [])
-            person.starships.each({
-                Starship starship = service.getStarshipFromUri(it)
-                starshipsByTatooineResident.get(person.name).add(starship.name)
-
+            executor.submit(new Runnable() {
+                @Override
+                void run() {
+                    Person person = service.getPersonFromUri(it)
+                    starshipsByTatooineResident.put(person.name, [])
+                    CountDownLatch peopleLatch = new CountDownLatch(person.starships.size())
+                    person.starships.each({
+                        executor.submit(new Runnable() {
+                            @Override
+                            void run() {
+                                Starship starship = service.getStarshipFromUri(it)
+                                starshipsByTatooineResident.get(person.name).add(starship.name)
+                            }
+                        })
+                        peopleLatch.countDown()
+                    })
+                    peopleLatch.await()
+                    tatooineLatch.countDown()
+                }
             })
         })
+        tatooineLatch.await()
+        executor.shutdown()
+        executor.awaitTermination(100, TimeUnit.SECONDS)
         println starshipsByTatooineResident
     }
 }
